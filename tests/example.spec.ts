103de7773a039d4aab23ec8cba6d3561d772231c
Andriy Pyvovarchuk <andriy.pyvovarchuk@techmagic.co>
2021-12-20 18:00:47 +0200

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,29 @@
 import { test, expect } from '@playwright/test';
 
-test('basic test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  const title = page.locator('.navbar__inner .navbar__title');
-  await expect(title).toHaveText('Playwright');
+test.use({ headless: false });
+
+test.describe('feature foo', () => {
+    
+  test.beforeAll(async ({}) => {
+    console.log('hook beforeAll');
+  });
+
+  test.beforeEach(async ({page}) =>  {
+    console.log('hook beforeEach');
+    await page.goto('https://playwright.dev/');
+  });
+
+  test('my test', async ({ page }) => {
+    console.log('hook test');
+    await expect(page).toHaveURL('https://playwright.dev/');
+  });
+
+  test.afterEach(async () =>  {
+    console.log('hook afterEach');
+  });
+
+  test.afterAll(async () =>  {
+    console.log('hook afterAll');
+  });
+  
 });
