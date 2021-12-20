73d4684593df1a55d35758c1988d2e0dd78dc22a
Andriy Pyvovarchuk <andriy.pyvovarchuk@techmagic.co>
2021-12-20 18:44:13 +0200

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,19 +1,25 @@
 import { test, expect } from '@playwright/test';
 
+test.use({
+  headless: false,
+  geolocation: {
+    latitude: 41.890221,
+    longitude: 12.492348
+  },
+  permissions: ['geolocation'],
+  locale: 'it-IT',
+  timezoneId: 'Europe/Rome'
+});
+
 test('test', async ({ page }) => {
 
-  await page.goto('https://www.google.com/');
+  await page.goto('https://www.google.com/maps');
 
-  await page.click('[aria-label="Пошук"]');
-
-  await page.fill('[aria-label="Пошук"]', 'playwright');
+  await page.goto('https://www.google.com/maps/@49.8441213,24.0680316,13z');
 
   await Promise.all([
     page.waitForNavigation(),
-    page.press('[aria-label="Пошук"]', 'Enter')
+    page.click('[aria-label="La tua posizione"]')
   ]);
-
-  await page.click('text=Playwright: Fast and reliable end-to-end testing for modern ...');
-  await expect(page).toHaveURL('https://playwright.dev/');
 
 });
