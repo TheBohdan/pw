216331b6032bde47909964029df8fdff1cdece0e
Andriy Pyvovarchuk <andriy.pyvovarchuk@techmagic.co>
2021-12-20 18:17:14 +0200

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,19 @@
 import { test, expect } from '@playwright/test';
 
-test('basic test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  const title = page.locator('.navbar__inner .navbar__title');
-  await expect(title).toHaveText('Playwright');
+test('test', async ({ page }) => {
+
+  await page.goto('https://www.google.com/');
+
+  await page.click('[aria-label="Пошук"]');
+
+  await page.fill('[aria-label="Пошук"]', 'playwright');
+
+  await Promise.all([
+    page.waitForNavigation(),
+    page.press('[aria-label="Пошук"]', 'Enter')
+  ]);
+
+  await page.click('text=Playwright: Fast and reliable end-to-end testing for modern ...');
+  await expect(page).toHaveURL('https://playwright.dev/');
+
 });
