76b016533095f6f87fb187e45165bd189e90b581
Andriy Pyvovarchuk <andriy.pyvovarchuk@techmagic.co>
2021-12-21 11:02:51 +0200

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,12 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, devices } from '@playwright/test';
+
+const Pixel3 = devices['Pixel 3 landscape'];
 
 test.use({
+  ...Pixel3,
+  colorScheme: 'dark',
   headless: false,
+  trace: 'retry-with-trace',
   geolocation: {
     latitude: 41.890221,
     longitude: 12.492348
@@ -15,11 +20,13 @@ test('test', async ({ page }) => {
 
   await page.goto('https://www.google.com/maps');
 
-  await page.goto('https://www.google.com/maps/@49.8441213,24.0680316,13z');
+  await page.click('button:has-text("Rimani nella versione web")');
 
   await Promise.all([
     page.waitForNavigation(),
-    page.click('[aria-label="La tua posizione"]')
+    page.click('.VYmeTd')
   ]);
+
+  await page.pause();
 
 });
