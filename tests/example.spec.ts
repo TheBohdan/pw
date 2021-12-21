8b36b27be8bf5572991a000f16c45e6a4a36c6f5
Andriy Pyvovarchuk <andriy.pyvovarchuk@techmagic.co>
2021-12-21 14:20:47 +0200

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,68 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
-test('basic test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  const title = page.locator('.navbar__inner .navbar__title');
-  await expect(title).toHaveText('Playwright');
+// Focus a test
+test.only('focus this test', async ({ page }) => {
+  // Run only focused tests in the entire project.
+});
+
+
+// Skip a test
+test.skip('skip this test', async ({ page }) => {
+  // This test is not run
+});
+
+// Conditionally skip a test
+test('skip this test', async ({ page, browserName }) => {
+  test.skip(browserName === 'firefox', 'Still working on it');
+});
+
+// Group tests
+test.describe('two tests', () => {
+  test('one', async ({ page }) => {
+    // ...
+  });
+
+  test('two', async ({ page }) => {
+    // ...
+  });
+});
+
+
+// Tag tests // npx playwright test --grep @fast
+test('Test login page @fast', async ({ page }) => {
+  // ...
+});
+
+test('Test full report @slow', async ({ page }) => {
+  // ...
+});
+
+// Conditionally skip a group of tests
+test.describe('chromium only', () => {
+  test.skip(({ browserName }) => browserName !== 'chromium', 'Chromium only!');
+
+  test.beforeAll(async () => {
+    // This hook is only run in Chromium.
+  });
+
+  test('test 1', async ({ page }) => {
+    // This test is only run in Chromium.
+  });
+
+  test('test 2', async ({ page }) => {
+    // This test is only run in Chromium.
+  });
+});
+
+// Use fixme in beforeEach hook
+
+test.beforeEach(async ({ page, isMobile }) => {
+  test.fixme(isMobile, 'Settings page does not work in mobile yet');
+
+  await page.goto('http://localhost:3000/settings');
+});
+
+test('user profile', async ({ page }) => {
+  await page.click('text=My Profile');
+  // ...
 });
