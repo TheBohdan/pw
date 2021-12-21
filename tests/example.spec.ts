839e69a4b22ebb2d9f3880cf5854fadfa4ae81a7
Andriy Pyvovarchuk <andriy.pyvovarchuk@techmagic.co>
2021-12-21 14:50:12 +0200

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,38 +1,41 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import { test, expect } from '@playwright/test';
 
 const REPO = 'pw';
 const USER = 'xmtigra';
 
-test('should create a bug report', async ({ request }) => {
-  const newIssue = await request.post(`/repos/${USER}/${REPO}/issues`, {
-    data: {
-      title: '[Bug] report 1',
-      body: 'Bug description',
-    }
+// Request context is reused by all tests in the file.
+let apiContext;
+
+test.beforeAll(async ({ playwright }) => {
+  apiContext = await playwright.request.newContext({
+    // All requests we send go to this API endpoint.
+    baseURL: 'https://api.github.com',
+    extraHTTPHeaders: {
+      // We set this header per GitHub guidelines.
+      'Accept': 'application/vnd.github.v3+json',
+      // Add authorization token to all requests.
+      // Assuming personal access token available in the environment.
+      'Authorization': `token ${process.env.API_TOKEN}`,
+    },
   });
-  expect(newIssue.ok()).toBeTruthy();
+})
 
-  const issues = await request.get(`/repos/${USER}/${REPO}/issues`);
-  expect(issues.ok()).toBeTruthy();
-  expect(await issues.json()).toContainEqual(expect.objectContaining({
-    title: '[Bug] report 1',
-    body: 'Bug description'
-  }));
-});
-
-test('should create a feature request', async ({ request }) => {
-  const newIssue = await request.post(`/repos/${USER}/${REPO}/issues`, {
+test('last created issue should be first in the list', async ({ page }) => {
+  const newIssue = await apiContext.post(`/repos/${USER}/${REPO}/issues`, {
     data: {
       title: '[Feature] request 1',
-      body: 'Feature description',
     }
   });
   expect(newIssue.ok()).toBeTruthy();
 
-  const issues = await request.get(`/repos/${USER}/${REPO}/issues`);
-  expect(issues.ok()).toBeTruthy();
-  expect(await issues.json()).toContainEqual(expect.objectContaining({
-    title: '[Feature] request 1',
-    body: 'Feature description'
-  }));
+  await page.goto(`https://github.com/${USER}/${REPO}/issues`);
+  const firstIssue = page.locator(`a[data-hovercard-type='issue']`).first();
+  await expect(firstIssue).toHaveText('[Feature] request 1');
+});
+
+test.afterAll(async ({ }) => {
+  // Dispose all responses.
+  await apiContext.dispose();
 });
