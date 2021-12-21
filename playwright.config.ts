10a4e07426adcbf79f937e46df952bacb736aaae
Andriy Pyvovarchuk <andriy.pyvovarchuk@techmagic.co>
2021-12-21 11:47:18 +0200

    Create playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,36 @@
+import { devices, PlaywrightTestConfig } from '@playwright/test';
+const config: PlaywrightTestConfig = {
+    projects: [
+        {
+            name: 'Desktop Chromium',
+            use: {
+                browserName: 'chromium',
+                viewport: { width: 1280, height: 720 },
+            },
+        },
+        {
+            name: 'Desktop Safari',
+            use: {
+                browserName: 'webkit',
+                viewport: { width: 1280, height: 720 },
+            }
+        },
+        {
+            name: 'Desktop Firefox',
+            use: {
+                browserName: 'firefox',
+                viewport: { width: 1280, height: 720 },
+            }
+        },
+        {
+            name: 'Mobile Chrome',
+            use: devices['Pixel 5'],
+        },
+        {
+            name: 'Mobile Safari',
+            use: devices['iPhone 12'],
+        }
+    ]
+};
+export default config;
+// npx playwright test --project "Mobile Safari" 
