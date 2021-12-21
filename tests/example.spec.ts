e4a135b71fb691469306f488cc9dd61cdb760b12
Andriy Pyvovarchuk <andriy.pyvovarchuk@techmagic.co>
2021-12-21 13:10:47 +0200

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,9 @@
 import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  const title = page.locator('.navbar__inner .navbar__title');
-  await expect(title).toHaveText('Playwright');
+  await page.goto('https://playwright.dev');
+  await page.click(".toggleTrack_32Fl"); // change color theme
+  expect(await page.screenshot()).toMatchSnapshot('landing.png');
 });
+
+// npx playwright test --update-snapshots
