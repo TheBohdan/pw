e84e3aacec3100e24db734712fd353a46994def8
Andriy Pyvovarchuk <andriy.pyvovarchuk@techmagic.co>
2021-12-21 13:23:42 +0200

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,3 +5,15 @@ test('basic test', async ({ page }) => {
   const title = page.locator('.navbar__inner .navbar__title');
   await expect(title).toHaveText('Playwright');
 });
+
+
+// npx playwright test --reporter=dot
+// npx playwright test --reporter=html
+
+// npx playwright show-report
+
+// PLAYWRIGHT_JSON_OUTPUT_NAME=results.json npx playwright test --reporter=json
+// PLAYWRIGHT_JUNIT_OUTPUT_NAME=results.xml npx playwright test --reporter=junit
+
+
+// PLAYWRIGHT_JUNIT_OUTPUT_NAME=results.xml npx playwright test --reporter=junit
