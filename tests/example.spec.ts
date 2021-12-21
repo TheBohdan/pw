389d8fd7883e5ab5296461be6debcf80516a70ee
Andriy Pyvovarchuk <andriy.pyvovarchuk@techmagic.co>
2021-12-21 15:22:41 +0200

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,31 @@
 import { test, expect } from '@playwright/test';
 
-test('basic test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  const title = page.locator('.navbar__inner .navbar__title');
-  await expect(title).toHaveText('Playwright');
-});
+test.describe.parallel('api' , async() => {
+  
+  test('get one persone', async ({ request }) => {
+    const response = await request.get(`https://swapi.dev/api/people/1`);
+    await expect(response.status()).toBe(200);
+    await expect(response.ok()).toBeTruthy();
+    
+    const res = await response.json();
+    
+    expect(res).toEqual(expect.objectContaining({
+      name: 'Luke Skywalker',
+      gender: 'male'
+    }));
+  });
+  
+  test('get people', async ({ request }) => {
+    const response = await request.get(`https://swapi.dev/api/people?page=2`);
+    await expect(response.status()).toBe(200);
+    await expect(response.ok()).toBeTruthy();
+    
+    const res = await response.json();
+    expect(res).toEqual(expect.objectContaining({
+      count: 82,
+      previous: 'https://swapi.dev/api/people/?page=1',
+      next: 'https://swapi.dev/api/people/?page=3',
+    }));
+  });
+  
+})
