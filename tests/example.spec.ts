9f43390fbaa6745621282f475442dd2517f9bd98
Bohdan Lozynskyi <lozynskyibb@gmail.com>
2023-03-01 14:02:14 +0200

    Modified network operations

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,12 +2,17 @@ import { test } from '@playwright/test';
 
 test.use({headless: false});
 
-test.beforeEach(async ({ context }) => {
+test.beforeEach(async ({ page }) => {
   // Block any css requests for each test in this file.
-  await context.route(/.css/, route => route.abort());
+  await page.route(/.css/, route => route.abort());
 });
 
 test('basic test', async ({ page }) => {
+  // Subscribe to 'request' and 'response' events.
+  page.on('request', 
+    request => console.log('>>', request.method(), request.url()));
+  page.on('response', 
+    response => console.log('<<', response.status(), response.url()));
   // Block png and jpeg images.
   await page.route(/(png|jpeg|svg)$/, route => route.abort());
 
