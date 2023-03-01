7e9496f863476e058c4d29113dbcb3a16685db3b
TheBohdan <lozynskyibb@gmail.com>
2023-03-01 02:54:14 +0200

    Updated page object selectors and model

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -1,4 +1,4 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 
 export class HomePage {
     private page: Page;
@@ -7,16 +7,18 @@ export class HomePage {
       this.page = page;
     }
 
-    async navigate() {
-      await this.page.goto('https://playwright.dev/');
+    get searchInput(): Locator { return this.page.locator('[aria-label="Search"]'); }
+    get docSearchInput(): Locator { return this.page.locator('[placeholder="Search docs"]'); }
+    get pageMainHeading(): Locator { return this.page.locator('h1'); }
+
+    navigate() {
+      return this.page.goto('https://playwright.dev/');
     }
     
-    async search(text) {
-      await this.page.fill('[placeholder="Search"]', text);
+    async search(text: string) {
+      await this.searchInput.click();
+      await this.docSearchInput.fill(text);
       await this.page.waitForSelector("text=Page Object Models");
-      await Promise.all([
-        this.page.waitForNavigation(),
-        this.page.press('[placeholder="Search"]', 'Enter')
-      ]);
+      await this.docSearchInput.press('Enter');
     }
   }
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,11 +1,15 @@
 import { test, expect } from '@playwright/test';
 import { HomePage } from '../pages/home.page';
 
-// test.use({headless: false})
+test.use({
+  headless: false,
+  browserName: 'chromium'
+})
 
 test('basic test', async ({ page }) => {
   const home = new HomePage(page);
   await home.navigate();
   await home.search('page object');
   await expect(page).toHaveURL('https://playwright.dev/docs/pom');
+  expect(await home.pageMainHeading.textContent()).toEqual('Page Object Models');
 });
