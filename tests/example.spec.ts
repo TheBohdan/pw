42a5597295408c0d58c2c63d32675e309c70a831
Bohdan Lozynskyi <lozynskyibb@gmail.com>
2023-03-01 15:59:52 +0200

    Added config file and image compare options

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,8 +3,9 @@ import { test, expect } from '@playwright/test';
 test('basic test', async ({ page }) => {
   await page.goto('https://playwright.dev');
   await page.click(".toggleButton_gllP"); // change color theme
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(15000);
   expect(await page.screenshot()).toMatchSnapshot('landing.png');
+  // expect(await page.screenshot()).toMatchSnapshot({ maxDiffPixelRatio: 0.02 });
 });
 
 // npx playwright test --update-snapshots
